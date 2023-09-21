f69886c53646e9beef350617aff3f884a51f8dc7
Himan-Miku <himanshu28.mj@gmail.com>
2023-09-21 18:32:27 +0530

    style: add basic layout structure

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,3 +1,12 @@
 export default function Home() {
-  return <main></main>;
+  return (
+    <main>
+      <div className="My-Drive"></div>
+      <div className="My-Drive-content"></div>
+      <div className="Folders"></div>
+      <div className="Folders-content"></div>
+      <div className="Files"></div>
+      <div className="Files-content"></div>
+    </main>
+  );
 }
