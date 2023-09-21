46f73e384f81bffff5c8ab85269d107172a4816c
Himan-Miku <himanshu28.mj@gmail.com>
2023-09-21 18:58:17 +0530

    style: add MyDrive component

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,11 @@
+import MyDrive from "@/components/MyDrive";
+
 export default function Home() {
   return (
     <main>
-      <div className="My-Drive"></div>
+      <div className="My-Drive">
+        <MyDrive />
+      </div>
       <div className="My-Drive-content"></div>
       <div className="Folders"></div>
       <div className="Folders-content"></div>
