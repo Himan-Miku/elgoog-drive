caa2c45fa287b6684760832369de809eb7d7a4da
Himan-Miku <himanshu28.mj@gmail.com>
2023-09-22 21:07:52 +0530

    refactor: better file tree

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,38 +1,9 @@
-use std::time::Duration;
-
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
-use aws_sdk_s3::{presigning::PresigningConfig, Client, Error};
+use aws_sdk_s3::Client;
 use serde::{Deserialize, Serialize};
+use utils::s3::{get_object_uri, show_objects};
 
-async fn show_objects(client: &Client, bucket: &str) -> Result<(), Error> {
-    let objects = client.list_objects_v2().bucket(bucket).send().await?;
-    println!("Objects are : ");
-    for obj in objects.contents().unwrap_or_default() {
-        println!("{:?}", obj.key().unwrap());
-    }
-
-    Ok(())
-}
-
-async fn get_object_uri(
-    client: &Client,
-    bucket: &str,
-    object_key: &str,
-    expires_in: u64,
-) -> Result<(), Box<dyn std::error::Error>> {
-    let expiry_time = Duration::from_secs(expires_in);
-
-    let presigned_request = client
-        .get_object()
-        .bucket(bucket)
-        .key(object_key)
-        .presigned(PresigningConfig::expires_in(expiry_time)?)
-        .await?;
-
-    println!("Object URI : {}", presigned_request.uri());
-
-    Ok(())
-}
+mod utils;
 
 #[derive(Debug, Serialize, Deserialize)]
 struct Intro {
