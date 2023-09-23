6300251add12279aae337cc5959010b9edebd137
Himan-Miku <himanshu28.mj@gmail.com>
2023-09-24 01:35:23 +0530

    feat: fetch folders from s3

--- frontend/src/components/FoldersContent.tsx
+++ frontend/src/components/FoldersContent.tsx
@@ -1,7 +1,34 @@
-"use client";
-
-const FoldersContent = () => {
-  return <div>{}</div>;
+type foldersData = {
+  folders_vec: Array<string>;
 };
 
-export default FoldersContent;
+const fetchFolders = async () => {
+  try {
+    const res = await fetch("http://localhost:8000/api/fetchFolders");
+    if (res.ok) {
+      const data = (await res.json()) as foldersData;
+      console.log(data);
+      return data;
+    } else {
+      console.error("Fetch failed with status:", res.status);
+      throw new Error("Fetch failed");
+    }
+  } catch (error) {
+    console.error("Fetch error:", error);
+    throw error;
+  }
+};
+
+export default async function FoldersContent() {
+  const data = await fetchFolders();
+
+  console.log("data from fn : ", data);
+
+  return (
+    <div>
+      {data.folders_vec?.map((folName, index) => {
+        return <h1 key={index}>{folName}</h1>;
+      })}
+    </div>
+  );
+}
