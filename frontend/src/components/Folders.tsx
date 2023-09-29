036de9014d0fec092513171ca4a53a77d699e6d3
Himan-Miku <himanshu28.mj@gmail.com>
2023-09-29 23:26:31 +0530

    feat: add folders fetching fn 🔥

--- frontend/src/components/Folders.tsx
+++ frontend/src/components/Folders.tsx
@@ -1,5 +1,37 @@
-const Folders = () => {
-  return <div>Folders</div>;
+import FoldersContent from "./FoldersContent";
+
+export type foldersData = {
+  folders_vec: Array<string>;
 };
 
-export default Folders;
+const fetchFolders = async () => {
+  try {
+    const res = await fetch("http://localhost:8000/api/fetchFolders");
+    if (res.ok) {
+      const data = (await res.json()) as foldersData;
+      console.log(data);
+      return data;
+    } else {
+      console.error("Fetch failed with status:", res.status);
+      throw new Error("Fetch failed");
+    }
+  } catch (error) {
+    console.error("Fetch error:", error);
+    throw error;
+  }
+};
+
+export default async function Folders() {
+  const data = await fetchFolders();
+
+  console.log("data from fn : ", data);
+
+  return (
+    <>
+      <div className="px-11 py-2 font-semibold text-[#e6e6e6]">Folders</div>
+      <div className="flex gap-6 px-8 py-3">
+        <FoldersContent data={data} />
+      </div>
+    </>
+  );
+}
