f56e6a54e5e5a0026941c4ff220aaa6cb27d5593
Himan-Miku <himanshu28.mj@gmail.com>
2023-09-29 23:25:54 +0530

    refactor: rm unnecessary placeholder structure 🗑️

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,4 @@
 import Folders from "@/components/Folders";
-import FoldersContent from "@/components/FoldersContent";
 import MyDrive from "@/components/MyDrive";
 import MyDriveContent from "@/components/MyDriveContent";
 
@@ -12,14 +11,9 @@ export default function Home() {
       <div className="My-Drive-content">
         <MyDriveContent />
       </div>
-      <div className="Folders">
+      <div className="Folders-content">
         <Folders />
       </div>
-      <div className="Folders-content">
-        <FoldersContent />
-      </div>
-      <div className="Files"></div>
-      <div className="Files-content"></div>
     </main>
   );
 }
