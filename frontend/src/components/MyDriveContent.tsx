d36773f8e72dd0d287196cdb6b2141de26cb11a5
Himan-Miku <himanshu28.mj@gmail.com>
2023-09-29 23:28:17 +0530

    wip: fetch_all_objects fn

--- frontend/src/components/MyDriveContent.tsx
+++ frontend/src/components/MyDriveContent.tsx
@@ -1,5 +1,25 @@
-const MyDriveContent = () => {
-  return <div className="px-8 pb-2">MyDrive Content</div>;
+const list_objects = async () => {
+  const res = await fetch("http://localhost:8000/api/getObjects", {
+    cache: "no-store",
+  });
+  const data = (await res.json()) as Array<string>;
+  return data;
+};
+
+const MyDriveContent = async () => {
+  const data = await list_objects();
+
+  return (
+    <div className="px-8 py-2 grid grid-cols-4 gap-4">
+      {data?.map((obj, index) => {
+        return (
+          <div key={index} className="w-full bg-custom-nav p-3 rounded-xl">
+            {obj}
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default MyDriveContent;
