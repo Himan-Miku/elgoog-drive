d395f4b406fe4c70952c617c44a7bbcc475ae90f
Himan-Miku <himanshu28.mj@gmail.com>
2023-09-29 13:00:26 +0530

    refactor: add structs in a separate file 🦀

---
+++ backend/src/structs/structs.rs
@@ -0,0 +1,20 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Metadata {
+    pub name: String,
+    pub content_type: String,
+    pub size: u32,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct FolderName {
+    #[serde(rename = "folderName")]
+    pub folder_name: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct FoldersArray {
+    pub folders_vec: Vec<String>,
+}
