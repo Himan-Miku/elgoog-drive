be325a32dc7160ad3f566627ff6398acee4a6378
Himan-Miku <himanshu28.mj@gmail.com>
2023-10-01 12:51:09 +0530

    refactor: add ObjectCards component 🪄

--- frontend/src/components/MyDriveContent.tsx
+++ frontend/src/components/MyDriveContent.tsx
@@ -1,5 +1,5 @@
 import { getServerSession } from "next-auth";
-import Image from "next/image";
+import ObjectCards from "./ObjectCards";
 
 const list_objects = async (name: string) => {
   const res = await fetch(`http://localhost:8000/api/getObjects?name=${name}`, {
@@ -18,65 +18,7 @@ const MyDriveContent = async () => {
 
   return (
     <div className="px-8 py-2 grid grid-cols-5 gap-4">
-      {data?.map((obj, index) => {
-        let imageSrc;
-        let smImageSrc;
-
-        console.log("Object : ", obj);
-
-        if (obj.endsWith(".png") || obj.endsWith(".jpeg")) {
-          imageSrc = "/image-lg.png";
-          smImageSrc = "/image-sm.png";
-        } else if (obj.endsWith(".pdf")) {
-          imageSrc = "/pdf-lg.png";
-          smImageSrc = "/pdf-sm.png";
-        } else if (
-          obj.endsWith(".mp4") ||
-          obj.endsWith(".mov") ||
-          obj.endsWith(".wmv")
-        ) {
-          imageSrc = "/mp4-lg.png";
-          smImageSrc = "/mp4-sm.png";
-        } else if (
-          obj.endsWith(".mp3") ||
-          obj.endsWith(".wma") ||
-          obj.endsWith(".wav")
-        ) {
-          imageSrc = "/music-lg.png";
-          smImageSrc = "/music-sm.png";
-        } else {
-          imageSrc = "/docs-lg.png";
-          smImageSrc = "/docs-sm.png";
-        }
-
-        console.log("imageSrc:", imageSrc);
-        return (
-          <div
-            key={index}
-            className="w-full bg-custom-nav px-3 pt-2 pb-3 rounded-xl h-52 flex flex-col gap-2"
-          >
-            <div className="flex gap-2 px-2 py-2">
-              <Image
-                src={smImageSrc}
-                alt="small-image"
-                height={20}
-                width={24}
-                className="w-5"
-              />
-              <h3 className="text-[#e6e6e6] text-sm font-medium">
-                {obj.substring(obj.indexOf("/") + 1, obj.length).length > 18
-                  ? obj
-                      .substring(obj.indexOf("/") + 1, obj.length)
-                      .substring(0, 18) + "..."
-                  : obj.substring(obj.indexOf("/") + 1, obj.length)}
-              </h3>
-            </div>
-            <div className="w-full grid place-content-center flex-grow bg-custom-backg h-20 rounded-xl">
-              <Image src={imageSrc} alt="big-image" height={64} width={64} />
-            </div>
-          </div>
-        );
-      })}
+      <ObjectCards data={data} />
     </div>
   );
 };
