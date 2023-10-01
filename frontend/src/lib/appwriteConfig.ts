08f57d62ef0ba2cd23c36d7bd188ea58c1cf496d
Himan-Miku <himanshu28.mj@gmail.com>
2023-10-01 23:02:21 +0530

    fix: add appwrite credentials in .env file ⚙️

--- frontend/src/lib/appwriteConfig.ts
+++ frontend/src/lib/appwriteConfig.ts
@@ -1,11 +1,11 @@
 import { Client, Databases } from "appwrite";
 
 const client = new Client()
-  .setEndpoint("https://cloud.appwrite.io/v1")
-  .setProject("6516cefac5f24e866af2");
+  .setEndpoint(process.env.APPWRITE_ENDPOINT!)
+  .setProject(process.env.APPWRITE_PROJECT!);
 
 const databases = new Databases(client);
-const collectionId = "6519651a1c1aeec7a854";
-const databaseId = "651964d905efd4e67224";
+const collectionId = process.env.APPWRITE_COLLECTION;
+const databaseId = process.env.APPWRITE_DATABASE;
 
 export { databases, collectionId, databaseId };
