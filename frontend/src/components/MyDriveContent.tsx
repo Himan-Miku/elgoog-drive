1ff60d1dd2571190488ccca257b3b4e8a85f14cc
Himan-Miku <himanshu28.mj@gmail.com>
2023-10-30 22:07:08 +0530

    feat(firebase)!: use useEffect instead of firebase-react-hook 🔥

--- frontend/src/components/MyDriveContent.tsx
+++ frontend/src/components/MyDriveContent.tsx
@@ -1,21 +1,51 @@
 "use client";
 import ObjectCards from "./ObjectCards";
-import { useCollection } from "react-firebase-hooks/firestore";
 import { collectionRef } from "@/lib/utils/firebaseConfig";
 import { useSession } from "next-auth/react";
-import { query, where } from "firebase/firestore";
+import { onSnapshot, query, where } from "firebase/firestore";
+import { ResultsStore } from "@/context/MyDriveDataContext";
+import { firestoreData } from "./NewItem";
+import { useEffect } from "react";
+
+interface firestoreDataWithoutID {
+  name: string;
+  user: string;
+  contentType: string;
+  size: number;
+  isStarred: boolean;
+}
 
 const MyDriveContent = () => {
+  const { results, setResults } = ResultsStore();
   const { data: session } = useSession();
   let username = session?.user?.email?.split("@")[0] || "";
 
   const q = query(collectionRef, where("user", "==", username));
 
-  const [snapshot, loading, error] = useCollection(q);
+  useEffect(() => {
+    const unsubscribe = onSnapshot(q, (snapshot) => {
+      let updatedResults: Array<firestoreData> = [];
+
+      snapshot.docs.forEach((doc) => {
+        const data = {
+          id: doc.id,
+          ...(doc.data() as firestoreDataWithoutID),
+        };
+
+        updatedResults.push(data);
+      });
+
+      setResults(updatedResults);
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
 
   return (
     <div className="px-8 py-2 grid md:grid-cols-5 sm:grid-cols-3 grid-cols-2 gap-4">
-      <ObjectCards data={snapshot?.docs} />
+      <ObjectCards data={results} />
     </div>
   );
 };
