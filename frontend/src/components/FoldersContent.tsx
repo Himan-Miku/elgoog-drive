3711270b9d0d8dd0b87613ea5e5ec314a3f8f0b7
Himan-Miku <himanshu28.mj@gmail.com>
2023-10-31 22:58:22 +0530

    feat(s3,firebase): delete folder functionality 🔥

--- frontend/src/components/FoldersContent.tsx
+++ frontend/src/components/FoldersContent.tsx
@@ -1,32 +1,57 @@
+"use client";
 import Link from "next/link";
-import { foldersData } from "./Folders";
 import { FaFolder } from "react-icons/fa6";
 import { SlOptionsVertical } from "react-icons/sl";
+import { FolderDataWithID } from "./Folders";
+import { deleteDoc, doc } from "firebase/firestore";
+import { firestoreDb } from "@/lib/utils/firebaseConfig";
+import { deleteNotify } from "./ObjectCards";
 
 interface FoldersContentProps {
-  data: foldersData;
+  data: Array<FolderDataWithID>;
 }
 
 const FoldersContent = ({ data }: FoldersContentProps) => {
+  const deleteFolder = async (folderId: string, folderName: string) => {
+    let folName = folderName.replace(/\/$/, "");
+    const res = await fetch(
+      `http://localhost:8000/api/deleteFolder/${folName}`,
+      {
+        method: "DELETE",
+      }
+    );
+    if (res.ok) {
+      console.log(await res.text());
+
+      await deleteDoc(doc(firestoreDb, "folders", folderId));
+    } else {
+      console.log(res.text);
+    }
+
+    deleteNotify();
+  };
+
   return (
     <>
-      {data.folders_vec?.map((folName, index) => {
+      {data.map((folName) => {
+        let actualFolderName =
+          folName.folderName.split("/").filter(Boolean).pop() ||
+          folName.folderName.replace(/\/$/, "");
         return (
           <div
             className="flex gap-6 items-center py-3 px-4 hover:bg-custom-nav bg-custom-nav min-w-fit rounded-xl"
-            key={index}
+            key={folName.id}
           >
-            <Link
-              href={`/folders/${encodeURIComponent(
-                folName.replace(/\/$/, "")
-              )}`}
-            >
+            <Link href={`/folders/${actualFolderName}`}>
               <div className="flex gap-2 items-center">
                 <FaFolder />
-                <h3>{folName.replace(/\/$/, "")}</h3>
+                <h3>{actualFolderName}</h3>
               </div>
             </Link>
-            <div className="rounded-full p-1 hover:bg-custom-backg">
+            <div
+              onClick={() => deleteFolder(folName.id, folName.folderName)}
+              className="rounded-full p-1 hover:bg-custom-backg"
+            >
               <SlOptionsVertical />
             </div>
           </div>
