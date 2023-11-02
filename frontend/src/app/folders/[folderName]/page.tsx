7102ede27752a8a541b6b32b139987622adb4ff0
Himan-Miku <himanshu28.mj@gmail.com>
2023-11-02 22:29:05 +0530

    feat: file upload dropdown in folders route ⚡

--- frontend/src/app/folders/[folderName]/page.tsx
+++ frontend/src/app/folders/[folderName]/page.tsx
@@ -1,11 +1,12 @@
 "use client";
 import ObjectCards from "@/components/ObjectCards";
 import { collectionRef } from "@/lib/utils/firebaseConfig";
-import { onSnapshot, query, where } from "firebase/firestore";
+import { addDoc, onSnapshot, query, where } from "firebase/firestore";
 import { IoCaretForward } from "react-icons/io5";
+import { MdOutlineUploadFile } from "react-icons/md";
 import { useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
-import { firestoreData } from "@/components/NewItem";
+import { firestoreData, receivedMetadata } from "@/components/NewItem";
 import { firestoreDataWithoutID } from "@/components/MyDriveContent";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
@@ -48,15 +49,105 @@ const FolderPage = () => {
 
   return (
     <div className="pt-5 px-8 flex flex-col gap-3">
-      <div className="p-2 flex items-center justify-center w-52 min-w-fit text-xl font-semibold">
+      <div className="py-2 flex items-center justify-center w-52 min-w-fit text-xl font-semibold">
         <Link href={"/"}>
           <h5 className="text-custom-green p-2 hover:bg-custom-nav rounded-xl cursor-pointer">
             My Drive /{" "}
           </h5>
         </Link>
-        <div className="flex text-custom-blue gap-1 items-center justify-center hover:bg-custom-nav p-2 rounded-xl cursor-pointer">
-          <h5>{folName}</h5>
-          <IoCaretForward />
+
+        <div className="dropdown dropdown-right">
+          <label
+            tabIndex={0}
+            className="flex text-custom-blue gap-1 items-center justify-center hover:bg-custom-nav p-2 rounded-xl cursor-pointer"
+          >
+            <h5>{folName}</h5>
+            <IoCaretForward />
+          </label>
+          <ul
+            tabIndex={0}
+            className="dropdown-content z-[1] menu p-2 bg-custom-backg rounded-box w-[13.5rem] ml-3 text-custom-green shadow-small -translate-y-1"
+          >
+            <li>
+              <label className="flex gap-6 items-center">
+                <input
+                  type="file"
+                  onChange={async (e) => {
+                    if (e.target.files && e.target.files.length > 0) {
+                      console.log(e.target.files[0]);
+
+                      let file = e.target.files[0];
+
+                      const metadata = {
+                        name: file.name,
+                        contentType: file.type,
+                        size: file.size,
+                        user: session?.user?.email,
+                        sentFrom: pathname,
+                      };
+
+                      try {
+                        const response = await fetch(
+                          "http://localhost:8000/api/getMetadata",
+                          {
+                            method: "POST",
+                            headers: {
+                              "Content-Type": "application/json",
+                            },
+                            body: JSON.stringify(metadata),
+                          }
+                        );
+
+                        if (response.ok) {
+                          console.log("Metadata sent successfully");
+                          const uri_data: receivedMetadata =
+                            await response.json();
+                          console.log("uri_data from NewItem: ", uri_data);
+                          try {
+                            const response = await fetch(
+                              uri_data.presigned_put_uri,
+                              {
+                                method: "PUT",
+                                headers: {
+                                  "Content-Type": metadata.contentType,
+                                },
+                                body: e.target.files[0],
+                              }
+                            );
+
+                            if (response.ok) {
+                              console.log("File uploaded successfully!");
+                              await addDoc(collectionRef, {
+                                name: uri_data.obj_key,
+                                user: uri_data.user_name,
+                                size: file.size,
+                                contentType: file.type,
+                                isStarred: false,
+                                sentFrom: uri_data.sent_from,
+                              });
+                            } else {
+                              console.log("Error uploading file.");
+                            }
+                          } catch (error) {
+                            console.error("An error occurred:", error);
+                          }
+                        } else {
+                          console.log(
+                            `Metadata response error ${response.status}`
+                          );
+                        }
+                      } catch (error) {
+                        console.log("An error occured while sending metadata");
+                      }
+                    }
+                  }}
+                  className="w-full"
+                />
+                <MdOutlineUploadFile size={"1.5em"} />
+                <h5>File Upload</h5>
+              </label>
+            </li>
+          </ul>
         </div>
       </div>
       <div className="py-2 grid md:grid-cols-5 sm:grid-cols-3 grid-cols-2 gap-4">
