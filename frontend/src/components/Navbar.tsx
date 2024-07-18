34917c27f4a3d14e07bb23591219b7053168a253
sushant <raosushant045@gmail.com>
2024-07-18 21:06:43 +0530

    feat:responsive Sidebar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -8,6 +8,8 @@ import { usePathname } from "next/navigation";
 import clsx from "clsx";
 import { IResizeContext, resizeContext } from "@/context/ResizeContext";
 import { SidebarStore } from "@/context/SidebarContext";
+import SidebarItems from "./SidebarItems";
+import Sidebar from "./Sidebar";
 
 interface SearchResultData {
   readonly objectID: string;
@@ -53,12 +55,12 @@ const Navbar = () => {
   return (
     <div className="flex justify-between w-full items-center py-3 md:px-14 px-2 h-full">
       <div className="flex gap-3 min-w-fit md:w-[35rem] w-80 bg-custom-backg rounded-3xl px-3 py-1">
-        {isMobile ? (
+        <>
           <div
             onClick={() => {
               setShowSidebar(!showSidebar);
             }}
-            className="rounded-full min-w-fit p-2 group hover:bg-custom-nav"
+            className="md:hidden block rounded-full min-w-fit p-2 group hover:bg-custom-nav"
           >
             <svg
               width={24}
@@ -72,21 +74,26 @@ const Navbar = () => {
               <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"></path>
             </svg>
           </div>
-        ) : (
-          <div className="rounded-full min-w-fit p-2 group hover:bg-custom-nav">
-            <svg
-              width={24}
-              height={24}
-              viewBox="0 0 24 24"
-              focusable="false"
-              className={clsx("fill-[#e6e6e6] group-hover:fill-custom-green", {
-                "fill-gray-600": pathname != "/",
-              })}
-            >
-              <path d="M20.49,19l-5.73-5.73C15.53,12.2,16,10.91,16,9.5C16,5.91,13.09,3,9.5,3S3,5.91,3,9.5C3,13.09,5.91,16,9.5,16 c1.41,0,2.7-0.47,3.77-1.24L19,20.49L20.49,19z M5,9.5C5,7.01,7.01,5,9.5,5S14,7.01,14,9.5S11.99,14,9.5,14S5,11.99,5,9.5z"></path>
-            </svg>
-          </div>
-        )}
+          {showSidebar && (
+            <div className="absolute top-0 left-0  z-10">
+              <Sidebar />
+            </div>
+          )}
+        </>
+
+        <div className="hidden md:block rounded-full min-w-fit p-2 group hover:bg-custom-nav">
+          <svg
+            width={24}
+            height={24}
+            viewBox="0 0 24 24"
+            focusable="false"
+            className={clsx("fill-[#e6e6e6] group-hover:fill-custom-green", {
+              "fill-gray-600": pathname != "/",
+            })}
+          >
+            <path d="M20.49,19l-5.73-5.73C15.53,12.2,16,10.91,16,9.5C16,5.91,13.09,3,9.5,3S3,5.91,3,9.5C3,13.09,5.91,16,9.5,16 c1.41,0,2.7-0.47,3.77-1.24L19,20.49L20.49,19z M5,9.5C5,7.01,7.01,5,9.5,5S14,7.01,14,9.5S11.99,14,9.5,14S5,11.99,5,9.5z"></path>
+          </svg>
+        </div>
 
         <input
           type="search"
