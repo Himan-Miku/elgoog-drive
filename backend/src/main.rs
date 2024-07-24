92c59d9a493b352a858126be6279879ff9dc8d00
Himanshu Jagdale <himanshu28.mj@gmail.com>
2024-07-24 22:33:43 +0530

    feat(prom): add mem_usage & cpu_usage for monitoring

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -2,6 +2,10 @@ use actix_cors::Cors;
 use actix_web::{delete, get, post, web, App, HttpRequest, HttpResponse, HttpServer, Responder};
 use actix_web_prom::PrometheusMetricsBuilder;
 use aws_sdk_s3::Client;
+use prometheus::Gauge;
+use std::thread;
+use std::time::Duration;
+use systemstat::{Platform, System};
 use uuid::Uuid;
 
 use structs::structs::{
@@ -209,10 +213,45 @@ async fn remove_folder(path: web::Path<(String, String)>) -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    let sys = System::new();
+
     let prometheus = PrometheusMetricsBuilder::new("api")
         .endpoint("/metrics")
         .build()
         .unwrap();
+
+    let cpu_usage = Gauge::new("cpu_usage", "Current CPU usage in percent").unwrap();
+    let mem_usage = Gauge::new("mem_usage", "Current memory usage in percent").unwrap();
+
+    prometheus
+        .registry
+        .register(Box::new(cpu_usage.clone()))
+        .unwrap();
+
+    prometheus
+        .registry
+        .register(Box::new(mem_usage.clone()))
+        .unwrap();
+    thread::spawn(move || loop {
+        match sys.cpu_load_aggregate() {
+            Ok(cpu) => {
+                thread::sleep(Duration::from_secs(1));
+                let cpu = cpu.done().unwrap();
+                cpu_usage.set(f64::trunc(
+                    ((cpu.system * 100.0) + (cpu.user * 100.0)).into(),
+                ));
+            }
+            Err(x) => println!("\nCPU load: error: {}", x),
+        }
+        match sys.memory() {
+            Ok(mem) => {
+                let memory_used = mem.total.0 - mem.free.0;
+                let pourcentage_used = (memory_used as f64 / mem.total.0 as f64) * 100.0;
+                mem_usage.set(f64::trunc(pourcentage_used));
+            }
+            Err(x) => println!("\nMemory: error: {}", x),
+        }
+    });
 
     HttpServer::new(move || {
         let cors = Cors::permissive();
